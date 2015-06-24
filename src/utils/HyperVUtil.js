ffc502df6e834abd86f60c6d021608799eb6e79d
Magnus Udbjørg <magnus@udbjorg.net>
2015-06-24 21:36:40 +0200

    hyper-v init

---
+++ src/utils/HyperVUtil.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var util = require('./Util');
+var Promise = require('bluebird');
+
+var HyperVUtil = {
+  command: function () {
+   
+  },
+  filename: function () {
+    
+  },
+  checksum: function () {
+   
+  },
+  url: function () {
+    return ;
+  },
+  installed: function () {
+  
+  },
+  active: function () {
+   
+  },
+  version: function () {
+   
+  },
+  poweroffall: function () {
+  
+  },
+  mountSharedDir: function (vmName, pathName, hostPath) {
+  
+  },
+  killall: function () {
+  
+  },
+  vmstate: function (name) {
+  
+  }
+};
+
+module.exports = HyperVUtil;
